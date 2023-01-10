440e081b06ab4ce7ba025e677e141de4fbed4bb2
PAFiset <114942984+PAFiset@users.noreply.github.com>
2023-01-10 10:36:02 -0500

    personal page

--- elements/information.js
+++ elements/information.js
@@ -2,11 +2,24 @@ import styles from '../styles/PersonalInfo.module.scss'
 
 const PersonalInfo = () => {
 	return ( 
-		<form>
+		<form className={styles.info_form} >
 			<p className={styles.Form_title} >Personal info</p>
 			<p className={styles.Form_explanation}>
 				Please your name, email address and phone number.
 			</p>
+			<label for="name" >
+				Name
+			</label>
+			<input type="text" id="name" name="name" />
+			<label for="email">
+				Email Address
+			</label>
+			<input type="email" id="email" name="email" />
+			<label for="phone">
+				Phone Number
+			</label>
+			<input type="tel" id="phone" name="phone">
+			</input>
 		</form>
 	 );
 }
